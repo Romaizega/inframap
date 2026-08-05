5a206a30f619bb0a5ef4e15b9107b99b4a0bbcdf
Roman Izegov <izegovrg@yandex.ru>
2026-08-05 14:54:03 +0300

    added backend with Fastify and TS

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+
+const app = Fastify({
+    logger: true
+})
+
+app.get('/health', async () => {
+    return { status: 'ok', service: 'inframap-backend' }
+})
+
+const start = async () => {
+    try {
+        await app.listen({ port: 3001, host: '0.0.0.0' })
+    } catch (error) {
+        app.log.error(error)
+        process.exit(1)
+
+    }
+}
+
+start()
