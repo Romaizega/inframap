2f6c4a809293209d7617bcae4d13b055cdb13099
Roman Izegov <izegovrg@yandex.ru>
2026-08-13 09:34:25 +0300

    added guard and me controller

---
+++ apps/backend/src/modules/auth/auth_guard.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+declare module '@fastify/jwt' {
+    interface FastifyJWT {
+        payload: {id:string; email: string; role: string}
+        user: {id:string; email: string; role: string}
+    }
+}
+
+export const authenticate = async (
+    request: FastifyRequest,
+    reply: FastifyReply
+) => {
+    try {
+        await request.jwtVerify()
+    } catch (error) {
+        return reply.status(401).send({message: "Unauthorized"})
+    }
+}
