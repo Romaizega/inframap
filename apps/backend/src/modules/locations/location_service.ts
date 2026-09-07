dfd32b334274ab930abcfd774a3b411ea6809d20
Roman Izegov <izegovrg@yandex.ru>
2026-09-07 13:09:22 +0300

    added rack view

--- apps/backend/src/modules/locations/location_service.ts
+++ apps/backend/src/modules/locations/location_service.ts
@@ -1,74 +1,107 @@
 import prisma from "../../lib/prisma";
 import { CreateLocationDTO, UpdateLocationDTO } from "./location_schema";
 
-
-export const createLocation = async(data: CreateLocationDTO, organizationId: string) => {
-    return prisma.location.create({
-        data :{
-            ...data,
-            organizationId
-        }
-    })
-
-}
+export const createLocation = async (
+  data: CreateLocationDTO,
+  organizationId: string,
+) => {
+  return prisma.location.create({
+    data: {
+      ...data,
+      organizationId,
+    },
+  });
+};
 
 export const getLocations = async (organizationId: string) => {
-   const result = await prisma.location.findMany({
-        where: {
-            organizationId
+  const result = await prisma.location.findMany({
+    where: {
+      organizationId,
+    },
+    include: {
+      devices: {
+        select: {
+          status: true,
         },
-        include: {
-            devices:{
-                select: {
-                    status: true
-                }
-            }
-        }
-    })
-     console.log(JSON.stringify(result[0], null, 2))
-    return result
-}
+      },
+    },
+  });
+  console.log(JSON.stringify(result[0], null, 2));
+  return result;
+};
 
-export const getLocationById = async (id:string, organizationId: string) => {
-    const location = await prisma.location.findFirst({
-        where:{
-            id,
-            organizationId
-        }
-    })
-    if(!location) {
-        throw new Error ('Location not found')
-    }
-    return location
-}
+export const getLocationById = async (id: string, organizationId: string) => {
+  const location = await prisma.location.findFirst({
+    where: {
+      id,
+      organizationId,
+    },
+    select: {
+      id: true,
+      site: true,
+      building: true,
+      floor: true,
+      latitude: true,
+      longitude: true,
+      accessinstruction: true,
+      description: true,
+      createdAt: true,
+      updatedAt: true,
 
-export const updateLocation = async (data: UpdateLocationDTO, id: string, organizationId: string) => {
-    const location = await prisma.location.findFirst({
-        where: {
-            id, 
-            organizationId
-        }
-    })
-    if(!location) {
-        throw new Error ('Location not found')
-    }
-    return prisma.location.update({
-        where: {id},
-        data: {...data}
-    })
-}
+      devices: {
+        select: {
+          id: true,
+          name: true,
+          status: true,
+          rackUnit: true,
+          rackSize: true,
+          type: true,
+          ip_address: true,
+          manufacturer: true,
+          model: true,
+        },
+      },
+    },
+  });
 
-export const deleteLocation = async (id:string, organizationId: string) => {
-     const location = await prisma.location.findFirst({
-        where: {
-            id, 
-            organizationId
-        }
-    })
-    if(!location) {
-        throw new Error ('Location not found')
-    }
-    return prisma.location.delete({
-        where: {id}
-    })
-}
+  if (!location) {
+    throw new Error("Location not found");
+  }
+
+  return location;
+};
+
+export const updateLocation = async (
+  data: UpdateLocationDTO,
+  id: string,
+  organizationId: string,
+) => {
+  const location = await prisma.location.findFirst({
+    where: {
+      id,
+      organizationId,
+    },
+  });
+  if (!location) {
+    throw new Error("Location not found");
+  }
+  return prisma.location.update({
+    where: { id },
+    data: { ...data },
+  });
+};
+
+export const deleteLocation = async (id: string, organizationId: string) => {
+  const location = await prisma.location.findFirst({
+    where: {
+      id,
+      organizationId,
+    },
+  });
+  if (!location) {
+    throw new Error("Location not found");
+  }
+  return prisma.location.delete({
+    where: { id },
+  });
+};
